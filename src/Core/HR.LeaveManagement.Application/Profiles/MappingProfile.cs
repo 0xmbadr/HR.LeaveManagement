48bb1d76f9de76ba9f2b3ef94b7c1faddba7b82d
MrCarbene <Muhammad_Fouda@proton.me>
2023-07-02 14:12:47 +0200

    Updating MappingProfiles

--- src/Core/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ src/Core/HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
+using HR.LeaveManagement.Application.Dtos.LeaveAllocation;
 using HR.LeaveManagement.Application.Dtos.LeaveRequest;
+using HR.LeaveManagement.Application.Dtos.LeaveType;
 using HR.LeaveManagement.Application.LeaveAllocation.Dtos;
 using HR.LeaveManagement.Application.LeaveType.Dtos;
 using HR.LeaveManagement.Domain;
@@ -10,10 +12,23 @@ namespace HR.LeaveManagement.Application.Profiles
     {
         public MappingProfile()
         {
+            #region LeaveType Mappings
+            CreateMap<Domain.LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<Domain.LeaveType, CreateLeaveTypeDto>().ReverseMap();
+            #endregion
+
+            #region LeaveRequest Mappings
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
-            CreateMap<Domain.LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion
+
+            #region LeaveAllocation Mappings
             CreateMap<Domain.LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<Domain.LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<Domain.LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            #endregion
         }
     }
 }
