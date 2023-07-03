e914486b32562f5993e751e34a72d1f820d77fe2
MrCarbene <Muhammad_Fouda@proton.me>
2023-07-03 02:53:14 +0200

    Add LeaveTypes Controller

---
+++ src/API/HR.LeaveManagement.api/Controllers/LeaveTypesController.cs
@@ -0,0 +1,62 @@
+using HR.LeaveManagement.Application.Dtos.LeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using HR.LeaveManagement.Application.LeaveType.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.api.Controllers
+{
+    [Route("[api/controller]")]
+    [ApiController]
+    public class LeaveTypesController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public LeaveTypesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<LeaveTypeDto>>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypeListRequest());
+
+            return Ok(leaveTypes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveTypeDto>> Get(int id)
+        {
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailRequest { Id = id });
+
+            return Ok(leaveType);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateLeaveTypeDto leaveTypeDto)
+        {
+            var command = new CreateLeaveTypeCommand { CreateLeaveTypeDto = leaveTypeDto };
+            var response = await _mediator.Send(command);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] LeaveTypeDto leaveTypeDto)
+        {
+            await _mediator.Send(new UpdateLeaveTypeCommand { LeaveTypeDto = leaveTypeDto });
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteLeaveTypeCommand { Id = id });
+
+            return NoContent();
+        }
+    }
+}
